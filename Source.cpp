f42a3df2d3baddf46d234976160cb01a676e457f
Todireanu Laura <todireanulaura@gmail.com>
2025-02-28 21:41:55 +0200

    problema 3

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
+bool isPrime(int n)
+{
+    for (int tr = 2; tr < n / 2; tr++)
+        if ((n % tr ) == 0)
+            return false;
+    return true;
+}
 int main()
 {
-	cout << "primul lab oop";
-	return 0;
+    int n;
+    std::cout << "Enter a number:";
+    std::cin >> n;
+    if (isPrime(n))
+        std::cout << n << " is prime !";
+    else
+        std::cout << n << " is NOT prime !";
+    return 0;
 }
